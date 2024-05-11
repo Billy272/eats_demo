c89be9e9fde81f46f14f92f38ab3fad9259e9972
Billy272 <billylaroi@gmail.com>
2024-05-11 09:22:01 +0300

    Update text alignment in HomeScreen description

--- uber_eats_demo/lib/home_screen.dart
+++ uber_eats_demo/lib/home_screen.dart
@@ -35,7 +35,7 @@ class HomeScreen extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Text(
               description,
-              textAlign: TextAlign.center,
+              textAlign: TextAlign.left,
             ),
           ),
         ],
