4c4f4b77416407c273a0f52ebc4080fa488e7ce6
Billy272 <billylaroi@gmail.com>
2024-05-11 09:10:31 +0300

    commit

--- uber_eats_demo/lib/home_screen.dart
+++ uber_eats_demo/lib/home_screen.dart
@@ -9,15 +9,15 @@ class HomeScreen extends StatelessWidget {
         body: Column(
       children: <Widget>[
         _buildCard("Grocery", "Cheap", "Find grocery at a cheaper price.",
-            "https://images.unsplash.com/photo-1606780290001-4b3b3e3b3b3b"),
-        _buildCard("Grocerys", "Cheap", "The best grocerys in town.",
-            'https://images.unsplash.com/photo-1606780290001-4b3b3e3b3b3b'),
+            'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngtree.com%2Ffree-backgrounds-photos%2Fgroceries-pictures&psig=AOvVaw2RUHyoZj3JyL2PVEeuw2GD&ust=1715492177749000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNjtksjwhIYDFQAAAAAdAAAAABAJ'),
+        _buildCard("Burgers", "Cheap", "The best burgers in town.",
+            'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tastingtable.com%2F834391%2Fwhat-makes-restaurant-burgers-taste-different-from-homemade-burgers%2F&psig=AOvVaw1LKj3QK7qqttS43CUTOY3M&ust=1715492326255000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNDd4ozxhIYDFQAAAAAdAAAAABAE'),
       ],
     ));
   }
 
-  Widget _buildCard(String title, String subtitle, String description,
-      String imagePath, String imageUrl) {
+  Widget _buildCard(
+      String title, String subtitle, String description, String imageUrl) {
     return Card(
       child: Column(
         children: <Widget>[
